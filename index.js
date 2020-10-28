acdff95492551edfc2834dd1f3c2ad78ddf37bae
Paulos Liu <paulosliu@Pauloss-MacBook-Pro-2.local>
2020-10-28 01:01:18 -0700

    Added some validation functionality

--- index.js
+++ index.js
@@ -6,12 +6,29 @@
     }
 
     function InitNavButtons() {
-        $(".next-page-button").click((e) => {
+        $(".nav-button").click((e) => {
+            let isNext = $(e.target).hasClass("next-page-button");
             let $currPage = $(e.target).parents(".page");
             let index = parseInt($currPage.attr("page-index"));
 
-            let $nextPage = $(`.page[page-index='${index+1}']`);
+            if(isNext) {
+                let validated = ValidatePage(index);
+                if(!validated) {
+                    $("#warning").fadeIn();
+                    return;
+                } else {
+                    $("#warning").css("display", "none");
+                }
+            }
+
+            let nextIndex = isNext ? index + 1 : index - 1;
+            let $nextPage = $(`.page[page-index='${nextIndex}']`);
             $currPage.fadeOut(() => {$nextPage.fadeIn()});
         });
+    }
+
+    function ValidatePage(pageIndex) {
+
+        return true;
     }
 })();
