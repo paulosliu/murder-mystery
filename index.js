ac89e8e3bf2b384df95a19d9589fde2586a4b6af
Paulos Liu <paulosliu@gmail.com>
2020-10-29 00:23:43 -0700

    Added answers to all pages Genericized some code

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-(function() {
+(function () {
     $(OnLoad);
 
     function OnLoad() {
         InitNavButtons();
+        // ShowPage(3);
+    }
+
+    // for debugging
+    function ShowPage(index) {
+        $(".page").css("display", "none");
+        $(`.page[page-index='${index}']`).css("display", "block");
+        $("#game").fadeIn();
     }
 
     function InitNavButtons() {
         $(".start-button").click((e) => {
             let $currPage = $(e.target).parents(".page");
-            $currPage.fadeOut(() => {$("#game").fadeIn()});
+            $currPage.fadeOut(() => { $("#game").fadeIn() });
         });
-        
+
         $(".nav-button").click((e) => {
             $("#warning").css("display", "none");
             let isNext = $(e.target).hasClass("next-page-button");
             let $currPage = $(e.target).parents(".page");
             let index = parseInt($currPage.attr("page-index"));
 
-            if(isNext) {
+            if (isNext) {
                 let validated = ValidatePage(index);
-                if(!validated) {
+                if (!validated) {
                     $("#warning").fadeIn();
                     return;
+                } else {
+                    OnSuccess(index);
                 }
             }
 
             let nextIndex = isNext ? index + 1 : index - 1;
             let $nextPage = $(`.page[page-index='${nextIndex}']`);
-            $currPage.fadeOut(() => {$nextPage.fadeIn()});
+            $currPage.fadeOut(() => { $nextPage.fadeIn() });
+        });
+
+        $(".fake-nav-button").click((e) => {
+            $("#warning").css("display", "none");
+            $("#warning").fadeIn();
         });
     }
 
+    const ANS = ["ProSS", "skull", undefined, "Project Super Human", "Faculty Club", "61343"];
     function ValidatePage(pageIndex) {
-        switch (pageIndex)
-        {
-            case 1:
-                return ValidatePage1();
-            case 7:
-                return ValidatePage7();
-            default:
-                return true;        
-        }
-    }
-
-    function ValidatePage1() {
-        return $("#input-one").val() === "rickroll";
-    }
-
-    /**
-     * Faculty Building Code
-     */
-    function ValidatePage7()
-    {
-        if ($("#input-seven").val() === "61343")
-        {
-            document.getElementById('facultyBG').pause();
+        let value = ANS[pageIndex - 1];
+        if(typeof value === "undefined")
             return true;
-        }
-   
-        return false;
+
+        let $input = $(`.page[page-index='${pageIndex}'] input`);
+        return $input.val().toUpperCase() === value.toUpperCase();
+    }
+
+    function OnSuccess(pageIndex) {
+        if(pageIndex === 7)
+            document.getElementById('facultyBG').pause();
     }
 })();
